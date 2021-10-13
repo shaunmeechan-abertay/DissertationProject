ed9ff80d2c8525b4cf170d7025f03101751aa961
Shaun <1801006@uad.ac.uk>
2021-10-13 11:40:39 +0100

    Created enemy

---
+++ DissertationProject/Assets/Enemy.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    GameObject pathObject;
+    Transform targetPathNode;
+    int pathNodeIndex = 0;
+    float speed = 5;
+    Vector2 direction;
+    int health = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pathObject = GameObject.Find("Path");
+        direction = new Vector2(0, 0);
+    }
+
+    void getNextPathNode()
+    {
+        if(pathNodeIndex >= pathObject.transform.childCount)
+        {
+            reachedGoal();
+        }
+        else
+        {
+            Debug.Log("pathNodeIndex: " + pathNodeIndex);
+            targetPathNode = pathObject.transform.GetChild(pathNodeIndex);
+            pathNodeIndex++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(targetPathNode == null)
+        {
+            getNextPathNode();
+        }
+
+        direction = targetPathNode.position - transform.localPosition;
+        //This represents the distance we covered this frame
+        float disThisFrame = speed * Time.deltaTime;
+
+        if(direction.magnitude <= disThisFrame)
+        {
+            //We reached the node
+            targetPathNode = null;
+        }
+        else
+        {
+            //Move towards the node
+            transform.Translate(direction.normalized * disThisFrame, Space.World);
+            float z = Quaternion.LookRotation(direction).z;
+            transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, z, transform.rotation.w);
+        }
+
+    }
+
+    void reachedGoal()
+    {
+        Destroy(gameObject);
+    }
+}
